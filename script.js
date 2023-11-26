27d4ef064f9e22b1fa16123243ba7868f38d9d64
i-am-abdullah <heyiamabdullah@gmail.com>
2023-11-26 17:30:27 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn = document.querySelectorAll(".btn")
+let inp = document.querySelector("input")
+let str = ""
+Array.from(btn).forEach((bt)=>{
+bt.addEventListener("click",(e)=>{
+   if(e.target.innerHTML=="="){
+    str=eval(str)
+    inp.value = str
+   }
+
+   else if(e.target.innerHTML =="Clear"){
+    str=""
+    inp.value=str
+   }
+   else if(e.target.innerHTML =="DEL"){
+    str=str.substring(0,str.length-1)
+    inp.value=str
+   }
+    else{
+    console.log(e.target)
+    str+=e.target.innerHTML
+    inp.value = str}
+})
+})
